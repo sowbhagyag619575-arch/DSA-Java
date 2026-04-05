2b4cccf5fe23ca14a4915509e7786a4f73ff8210
sowbhagyag619575-arch <sowbhagyag619575@gmail.com>
2026-04-05 19:54:06 +0530

    Create SimpleArraySum.java

---
+++ SimpleArraySum.java
@@ -0,0 +1,10 @@
+public class SimpleArraySum{
+  public static int simpleArraySum(int [] ar){
+    int sum=0;
+    for(int i=0;i<ar.length;i++){
+      sum+=ar[i];
+    }
+    return sum;
+  }
+}
+    
